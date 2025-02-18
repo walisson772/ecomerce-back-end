87dfe3c302e47136fd24a96e71e36aca46152b31
Walisson Santos <walissonsantost008@gmail.com>
2025-02-18 19:27:09 -0300

    Projeto finalizado
    Esse é projeto bem simples para simular um sistema onde temos itens e valores, esses itens podem ser adicionados ao seu carrinho, o sistema tambem possui uma função para mostrar o item e o valor que tem no carrinho porem não pode adicionar mais de um produto talvez isso chague em uma atulização futura se eu me lembrar de faze-la

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import json
+import os
 
 PRODUTOS = [
     {'id':0, 'produto':'Poco x7 pro', 'preco':3_000},
@@ -12,43 +13,83 @@ ROOT_DIR = Path(__file__).parent
 FILE_JSON = 'user.json'
 FILE = ROOT_DIR / FILE_JSON
 
-class Ecommerce:
-    def __init__(self, produtos):
-        self.produtos = produtos
-        self.carrinho_item = 0
-        self.app_json()
-        
+    
 
-    def listar_produtos(self):
-        for produto in self.produtos:
+def listar_produtos(produtos):
+    print()
+    for produto in produtos:
+        print(f'{produto['id']}) Item: {produto['produto']} Preço: {produto['preco']}')
+    print()
+
+
+def filtrar_valores(produtos, min, max):
+    print()
+    print(f'Itens entre os valores {min} e {max}')
+    for produto in produtos:
+        if produto['preco'] >= min and produto['preco'] <= max:
             print(f'{produto['id']}) Item: {produto['produto']} Preço: {produto['preco']}')
+    print()
 
-    def filtrar_valores(self, min, max):
-        for produto in self.produtos:
-            if produto['preco'] >= min and produto['preco'] <= max:
-                print(f'{produto['id']}) Item: {produto['produto']} Preço: {produto['preco']}')
+def itens_json(produtos, file):
+    listar_produtos(produtos)
+    response = int(input('Digite o numero referente ao seu desejo: '))
+    carrinho = 0
+    for produto in produtos:
+        if response == produto['id']:
+            carrinho += produto['preco']
+            item = produto['produto']
+    values = [{'valor':carrinho, 'produto':item}]
+    with open(file, 'w', encoding='utf-8') as arquivo:
+        json.dump(values, arquivo, ensure_ascii=False, indent=2)
 
-    def app_json(self):
-        self.values = [
-                {
-                   'valor':self.carrinho_item
-            }   
-        ]
-        with open(FILE, 'w', encoding='utf-8') as arquivo:
-            json.dump(self.values, arquivo, ensure_ascii=False, indent=2)
+def ver_carrinho(file):
+    with open(file, 'r', encoding='utf-8') as arquivo:
+        produto = json.load(arquivo)
 
-    def cadastrar(self):
-        
+    for dados in produto:
+        produto = dados['produto']
+        valor = dados['valor']
+
+    print('<><><>' * 10)
+    print(f'Atualmente você tem \033[34m{produto}\033[0m em seu carrinho')
+    print(f'\033[34m{produto}\033[0m está custando \033[34m{valor}$\033[0m')    
+    print('<><><>' * 10)
 
 def opcoes():
-    print('1) Listar produtos')
-    print('2) Filtrar valores')
+    try:
+        print('\033[32m1) Filtrar valores\033[0m')
+        print('\033[32m2) Ver itens do carrinho\033[0m')
+        print('\033[32m3) Adicionar item ao carrinho\033[0m')
+        print('\033[32m4) Sair\033[0m')
+    except ValueError:
+        print('Digite apenas numeros')
     
     response = int(input('O que você deseja: '))
     return response
 
-ecommerce = Ecommerce(PRODUTOS)
-    
-# min = float(input('Digite o valor minimo: '))
-# max = float(input('Digite o valor maximo: '))
-# filtrar_valores(PRODUTOS,min, max)
+while True:
+    try:
+        response = opcoes()
+
+        if response == 1:
+            min = float(input('Digite o valor minimo: '))
+            max = float(input('Digite o valor maximo: '))
+            filtrar_valores(PRODUTOS, max=max, min=min)
+
+        elif response == 2:
+            ver_carrinho(FILE)
+
+        elif response == 3:
+            itens_json(PRODUTOS, FILE)
+
+        else:
+            os.system('cls')
+            print('Essa opção não existe')
+            continue
+    except ValueError:
+        os.system('cls')
+        print('\033[31mDigite apenas numeros referentes ao numero do item desejado\033[0m')
+
+    except FileNotFoundError:
+        os.system('cls')
+        print('\033[31mVocê não tem item nenhum no seu carrinho\033[0m')
